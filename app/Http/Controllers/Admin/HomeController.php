35cec23847f7fde19d66ad4e8e6d1d2a3f49323a
zestadore <contactzestadore@gmail.com>
2024-03-09 22:07:02 +0530

    Admin dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Payment;
 use App\Models\Quotation;
+use App\Models\Availability;
+use App\Models\Booking;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -20,7 +22,26 @@ class HomeController extends Controller
     public function dashboard()
     {
         $quotations=Quotation::where('status',0)->count();
-        return view('application.dashboard',['pendingQuotationCount'=>$quotations]);
+        $availability=Availability::where('status',0)->count();
+        $bookings=Booking::where('status',0)->count();
+        $payments=Payment::where('status',0)->count();
+        //get total payments added this month using created_at field
+        $paymentsThisMonth=Payment::where('created_at','>=',date('Y-m-d',strtotime('first day of this month')))->where('created_at','<=',date('Y-m-d',strtotime('last day of this month')))->count();
+        //get total approved payments added this month using created_at field
+        $approvedPaymentsThisMonth=Payment::where('status',1)->where('created_at','>=',date('Y-m-d',strtotime('first day of this month')))->where('created_at','<=',date('Y-m-d',strtotime('last day of this month')))->count();
+        //get total pending payments added this month using created_at field
+        $pendingPaymentsThisMonth=Payment::where('status',0)->where('created_at','>=',date('Y-m-d',strtotime('first day of this month')))->where('created_at','<=',date('Y-m-d',strtotime('last day of this month')))->count();
+        //get total rejected payments added this month using created_at field
+        $rejectedPaymentsThisMonth=Payment::where('status',2)->where('created_at','>=',date('Y-m-d',strtotime('first day of this month')))->where('created_at','<=',date('Y-m-d',strtotime('last day of this month')))->count();
+        return view('application.dashboard',
+            ['pendingQuotationCount'=>$quotations,
+            'pendingAvailabilityCount'=>$availability,
+            'pendingBookingCount'=>$bookings,
+            'pendingPaymentCount'=>$payments,
+            'paymentsThisMonth'=>$paymentsThisMonth,
+            'approvedPaymentsThisMonth'=>$approvedPaymentsThisMonth,
+            'pendingPaymentsThisMonth'=>$pendingPaymentsThisMonth,
+            'rejectedPaymentsThisMonth'=>$rejectedPaymentsThisMonth]);
     }
 
     public function changePassword()
--- resources/views/application/dashboard.blade.php
+++ resources/views/application/dashboard.blade.php
@@ -21,5 +21,3 @@
             @endcan
         </section>
     @endsection
-    
-    
